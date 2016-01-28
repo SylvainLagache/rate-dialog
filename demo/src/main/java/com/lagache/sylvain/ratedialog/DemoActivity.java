58b06a2ff19a3272be728e22b3db6adccf9dec44
Sylvain Lagache <sylvain.lagache.92@gmail.com>
2016-01-28 11:02:14 +1100

    Project creation with two modules (demo and library)

---
+++ demo/src/main/java/com/lagache/sylvain/ratedialog/DemoActivity.java
@@ -0,0 +1,14 @@
+package com.lagache.sylvain.ratedialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.lagache.sylvain.library.fragments.RateDialogFragment;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
---
+++ library/src/main/java/com/lagache/sylvain/library/fragments/RateDialogFragment.java
@@ -0,0 +1,7 @@
+package com.lagache.sylvain.library.fragments;
+
+/**
+ * Created by user on 28/01/2016.
+ */
+public class RateDialogFragment {
+}
